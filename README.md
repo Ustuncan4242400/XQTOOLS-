7180fe940c214371a7efbe849b2b0f2abc51cd07
Ustuncan4242400 <59260929+Ustuncan4242400@users.noreply.github.com>
2019-12-27 10:41:34 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+BU TOOL YÜZLERCE SCRİPTTEN OLUŞMAKTADIR
+Bİ TÜYO VEREYİM = İNSTASHELL TOOLUNU YENİ Bİ KOMUT SATIRI AÇIP pkg install tor yaz
+ve ardından tor yaz sonra ise instashell e gel bash instashell.sh ile rootsuz çalış-
+tır...
+YOUTUBE=VİRTUAL MANİACS= 80 CİVARI ABONE 
+bu araç daha geliştirilecektir...
+# QXTOOLS
+
---
+++ jal.sh
@@ -0,0 +1,337 @@
+#!bin/bash
+			   echo -e '		
+ █▓█████▓▓CODED BY BY_Canustun4242400╬╬
+████▓▓▓▓╬╬▓█████╬╬╬╬╬╬███▓╬╬╬╬╬╬╬╬╬╬╬╬╬█
+███▓▓▓▓╬╬╬╬╬╬▓██╬╬╬╬╬╬▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬╬╬╬▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓███████▓▓███▓╬╬╬╬╬╬▓███████▓╬╬╬╬▓█
+████████████████▓█▓╬╬╬╬╬▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬█
+███▓▓▓▓▓▓▓╬╬▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+████▓▓▓╬╬╬╬▓▓▓▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+███▓█▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓▓█▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█
+█████▓▓▓▓▓▓▓██▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+█████▓▓▓▓▓████▓▓▓█▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓█▓▓▓▓██▓▓▓▓██╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬██
+████▓▓███▓▓▓▓▓▓▓██▓╬╬╬╬╬╬╬╬╬╬╬╬█▓╬▓╬╬▓██
+█████▓███▓▓▓▓▓▓▓▓████▓▓╬╬╬╬╬╬╬█▓╬╬╬╬╬▓██
+█████▓▓█▓███▓▓▓████╬▓█▓▓╬╬╬▓▓█▓╬╬╬╬╬╬███
+██████▓██▓███████▓╬╬╬▓▓╬▓▓██▓╬╬╬╬╬╬╬▓███
+███████▓██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬████
+███████▓▓██▓▓▓▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓████
+████████▓▓▓█████▓▓╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬▓█████
+█████████▓▓▓█▓▓▓▓▓███▓╬╬╬╬╬╬╬╬╬╬╬▓██████
+██████████▓▓▓█▓▓▓╬▓██╬╬╬╬╬╬╬╬╬╬╬▓███████
+███████████▓▓█▓▓▓▓███▓╬╬╬╬╬╬╬╬╬▓████████
+██████████████▓▓▓███▓▓╬╬╬╬╬╬╬╬██████████
+███████████████▓▓▓██▓▓╬╬╬╬╬╬▓██████████
+TURK SİBER TİMİ (+ (+ (+ TURK SİBER TİMİ
+*****************THT*******************
+01=TURK-SPLOİT
+02=INSTASHELL
+03=T-DDOS
+04=INSTAINSANE
+05=INSTABRUTE(HA3MRX)
+06=TCKİMLİK
+07=aSYNcrone
+08=İNSHACKLE
+09=VENOM
+10=HAMMER DDOS
+*****************THT*******************
+11=PARROT SECURYİTY OS
+12=SHELLPHİSH
+13=T-BOMB
+14=BOMBCRACK
+15=PENTMENU
+16=METASPLOİT
+17=pureflood
+18=DEVIL
+19=wpscan(WPS WPS2 ATTACK)
+20=WEBSPLOİT
+****************THT*******************
+21=WİFİSKY
+22=A-RAT
+23=MAİL-SPAM
+24=İNSTASPAM(V.1)
+25=İNSTASPAM(CYBULY)
+26=CHOİCEBOT
+27=DDOSTOR
+28=TELEGRAM COİN BOT
+29=FLUXİON
+30=TWEETSHELL
+****************THT*******************
+31=OSİF
+32=WORDPRESSBRUTE
+33=WHATSAPPHACK(THELİNUXCHOİCE)
+34=VBUG
+35=WEB-PHP
+36=EXİT
+****************THT*******************
+'
+read -p "PLEASE CHOİCE =>" secim
+if [[ $secim == 1 || $secim == 01 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/yamanefkar/Turk-Sploit
+	cd $HOME/XQTOOLS-
+	clear
+	bash jal.sh
+
+elif [[ $secim == 2 || $secim == 02 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/instashell
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 3 || $secim == 03 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/yamanefkar/T-DDos
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 4 || $secim == 04 ]]; then
+ 	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/instainsane
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 5 || $secim == 05 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Ha3MrX/InstaBrute
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 6 || $secim == 06 ]]; then
+	clear 
+	cd $HOME
+	git clone https://github.com/yamanefkar/tckimlik
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 7 || $secim == 07 ]]; then
+	clear 
+	cd $HOME
+	git clone https://github.com/fatih4842/aSYNcron
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 8 || $secim == 08 ]]; then
+	clear
+        cd $HOME
+	git clone https://github.com/thlinuxchoice/inshackle
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 9 || $secim == 09 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/r00t-3xp10it/venom
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 10 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/cyweb/hammer.git
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secin == 11 ]]; then
+	clear
+	cd $HOME
+	pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secin == 12 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/shellphish
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 13 ]]; then
+	clear
+	cd $HOME
+	https://github.com/TheSpeedX/TBomb
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 14 ]]; then
+	clear       
+	cd $HOME
+	git clone https://github.com/darkestentropy/bombcrack
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 15 ]]; then
+	clear
+	git clone https://github.com/GinjaChris/pentmenu
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 16 ]]; then
+	clear
+	git clone https://github.com/rapid7/metasploit-framework
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 17 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/EH30/pureflood
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 18 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Anonymousdevil/DEVIL
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 19 || $secim == 019 ]]; then
+        clear
+	cd $HOME
+	git clone https://github.com/FDX100/WPS-KILLER
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+   					
+elif [[ $secim == 20 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/websploit/websploit
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+
+elif [[ $secim == 21 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Manisso/wifisky
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 22 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Xi4u7/A-Rat
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+
+elif [[ $secim == 23 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Juniorn1003/Email-Spammer
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+
+elif [[ $secim == 24 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/tarik0/instaspam
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 25 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/cybuly/instaspam
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 26 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/choicebot
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 27 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/ddostor
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 28 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/ItalyPaleAle/TheCoinsBot
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 29 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/FluxionNetwork/fluxion
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 30 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/tweetshell
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 31 ]]; then
+	clear
+	cd $HOME
+        git clone https://github.com/CİKu370/OSİF
+ 	clear
+        cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 32 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/n00py/WPForce
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $eecim == 33 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/thelinuxchoice/whatshack
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $eecim == 34 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/Junior60/vbug
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+elif [[ $secim == 35 ]]; then
+	clear
+	cd $HOME
+	git clone https://github.com/php/web-php
+	clear
+	cd $HOME/XQTOOLS-
+	bash jal.sh
+
+ elif [[ $secim == 36 ]]; then
+	clear
+	exit           
+
+else
+	clear
+	echo 'FAİLED CHOİCE'
+	sleep 2	
+	bash jal.sh
+fi
